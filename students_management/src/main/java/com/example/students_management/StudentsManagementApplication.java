2ea4ec4ccec62409b31353c0bfc22dc2d40351c2
mohamed-ait <mohamedaitlahcen62@gmail.com>
2022-04-23 16:07:06 +0000

    create instances of users and roles

--- students_management/src/main/java/com/example/students_management/StudentsManagementApplication.java
+++ students_management/src/main/java/com/example/students_management/StudentsManagementApplication.java
@@ -3,6 +3,7 @@ package com.example.students_management;
 import com.example.students_management.entities.Etudiant;
 import com.example.students_management.entities.Genre;
 import com.example.students_management.repositories.EtudiantRepository;
+import com.example.students_management.security.service.SecurityService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,7 +17,7 @@ public class StudentsManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentsManagementApplication.class, args);
 	}
-	@Bean
+	//@Bean
   CommandLineRunner	start(EtudiantRepository etudiantRepository){
 		return args->{
 			etudiantRepository.save(new Etudiant(null,"nom1","prenom1","prenom1nom1@gmail.com",new Date(), Genre.MASCULIN,true));
@@ -26,4 +27,21 @@ public class StudentsManagementApplication {
 
 		};
   }
+	@Bean
+	CommandLineRunner saveUsers(SecurityService securityService){
+		return args->{
+			//create users
+           securityService.saveNewUser("med","med123","med123");
+           securityService.saveNewUser("moha","med123","med123");
+           securityService.saveNewUser("medo","med123","med123");
+		   //create roles
+           securityService.saveNewRole("ADMIN","admin role description");
+           securityService.saveNewRole("USER","user role description");
+		   //add roles to users
+           securityService.addRoleToUser("med","ADMIN");
+           securityService.addRoleToUser("med","USER");
+           securityService.addRoleToUser("medo","USER");
+           securityService.addRoleToUser("moha","USER");
+		};
+	}
 }
