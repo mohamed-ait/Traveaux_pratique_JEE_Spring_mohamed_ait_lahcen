2e5fa99e657a9a23cea5f8e1771a9c6ba39f5c9b
mohamed-ait <mohamedaitlahcen62@gmail.com>
2022-04-23 15:46:55 +0000

    create SecurityService implementation class

--- students_management/src/main/java/com/example/students_management/security/service/SecurityService.java
+++ students_management/src/main/java/com/example/students_management/security/service/SecurityService.java
@@ -4,7 +4,7 @@ package com.example.students_management.security.service;
 import com.example.students_management.security.entities.AppRole;
 import com.example.students_management.security.entities.AppUser;
 
-public interface SecurityService {
+public interface SecurityService  {
     AppUser saveNewUser(String username , String password, String rePassword);
     AppRole saveNewRole(String roleName, String description);
     void addRoleToUser(String username,String roleName);
---
+++ students_management/src/main/java/com/example/students_management/security/service/SecurityServiceImpl.java
@@ -0,0 +1,77 @@
+package com.example.students_management.security.service;
+
+import com.example.students_management.security.entities.AppRole;
+import com.example.students_management.security.entities.AppUser;
+import com.example.students_management.security.repositories.AppRoleRepository;
+import com.example.students_management.security.repositories.AppUserRepository;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+
+@Service
+@Slf4j
+@Transactional
+@NoArgsConstructor
+public class SecurityServiceImpl implements SecurityService {
+    private AppUserRepository appUserRepository;
+    private AppRoleRepository appRoleRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public SecurityServiceImpl(AppUserRepository appUserRepository, AppRoleRepository appRoleRepository, PasswordEncoder passwordEncoder) {
+        this.appUserRepository = appUserRepository;
+        this.appRoleRepository = appRoleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public AppUser saveNewUser(String username, String password, String rePassword) {
+        if(!password.equals(rePassword)) throw new RuntimeException("password no match");
+        String hashedPassword=passwordEncoder.encode(password);
+        AppUser appUser=new AppUser();
+        appUser.setUserId(UUID.randomUUID().toString());
+        appUser.setUserName(username);
+        appUser.setPassword(hashedPassword);
+        appUser.setActive(true);
+        AppUser savedAppUser=appUserRepository.save(appUser);
+        return savedAppUser;
+    }
+
+    @Override
+    public AppRole saveNewRole(String roleName, String description) {
+        AppRole appRole=appRoleRepository.findByRoleName(roleName);
+        if(appRole!=null) throw new RuntimeException("role "+roleName+"already exist");
+        appRole=new AppRole();
+        appRole.setRoleName(roleName);
+        appRole.setDescription(description);
+        AppRole savedRole=appRoleRepository.save(appRole);
+        return savedRole;
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        AppRole appRole=appRoleRepository.findByRoleName(roleName);
+        if(appRole==null) throw new RuntimeException("role not found");
+        AppUser appUser=appUserRepository.findByUsername(username);
+        if(appUser==null) throw new RuntimeException("user not found");
+        appUser.getRoles().add(appRole);
+    }
+
+    @Override
+    public void removeRoleFromUser(String username, String roleName) {
+        AppRole appRole=appRoleRepository.findByRoleName(roleName);
+        if(appRole==null) throw new RuntimeException("role not found");
+        AppUser appUser=appUserRepository.findByUsername(username);
+        if(appUser==null) throw new RuntimeException("user not found");
+        appUser.getRoles().remove(appRole);
+    }
+
+    @Override
+    public AppUser loadUserByUserName(String username) {
+        return appUserRepository.findByUsername(username);
+    }
+}
